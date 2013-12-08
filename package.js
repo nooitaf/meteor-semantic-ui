0011c0cdb496055f4fc205543fd1235e84cb8116
nooitaf <nooitaf@gmail.com>
2013-12-08 23:08:25 +0100

    package fix
    should have been more explicit

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
 
-  api.use('jquery');
+  api.use('jquery','client');
 
   var path = Npm.require('path');
   var assetPath = path.join('lib/semantic-ui/build/uncompressed/');
