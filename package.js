d9eb99499a5c7db46244064a7bbc77a141ccec4d
nooitaf <nooitaf@gmail.com>
2014-12-24 12:02:21 +0100

    version bump 1.4.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Semantic UI packaged for Meteor",
-  version: "1.3.2",
+  version: "1.4.1",
   name: "nooitaf:semantic-ui",
   git: "https://github.com/nooitaf/meteor-semantic-ui.git"
 });
